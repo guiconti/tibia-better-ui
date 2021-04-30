c1e0d83ebbb19c5bf8ffb2b4508dc42028e38ece
unknown <guibasconti@gmail.com>
2021-04-30 02:38:43 -0400

    Fix key event handling

--- server/core/registerShortcuts.ts
+++ server/core/registerShortcuts.ts
@@ -1,8 +1,47 @@
 import { globalShortcut } from 'electron';
 import fs from 'fs';
 import path from 'path';
+import robot from 'robotjs';
 import getWindow from './getAppWindow';
 import { resizeWindow } from './windowSetup';
+import { ipcMain } from 'electron';
+import { events } from '../constants';
+
+robot.setKeyboardDelay(0);
+
+const registerKey = (event: string, hotkey: string) => {
+  if (globalShortcut.isRegistered(hotkey)) {
+    console.error('Duplicate hotkey error');
+    return;
+  }
+  const module = {
+    event,
+    hotkey,
+    registerFunction: async function() {
+      if (!this) {
+        return;
+      }
+      const window = getWindow();
+      globalShortcut.unregister(this.hotkey);
+      window.webContents.send(this.event, this.hotkey);
+      const adjustedKeys = this.hotkey.replace('CommandOrControl', 'control').toLowerCase();
+      const keys = adjustedKeys.split('+');
+      const key = keys[keys.length - 1];
+      let modifiers = [];
+      // keys.length - 1 because the last key is not a modifier
+      for (let i = 0; i < keys.length - 1; i++) {
+        modifiers.push(keys[i])
+      }
+      robot.keyTap(key, modifiers);
+    },
+  };
+  globalShortcut.register(hotkey, module.registerFunction.bind(module));
+};
+
+// Called from webpage so we can reregister the hotkey after the event is done
+ipcMain.handle(events.REGISTER_HOTKEY, async (_event, ...args) => {
+  registerKey(args[0], args[1]);
+});
 
 export default async () => {
   const window = getWindow();
@@ -11,7 +50,7 @@ export default async () => {
     const [width, height] = window.getSize();
     const [x, y] = window.getPosition();
     window.reload();
-    resizeWindow({width, height, x, y});
+    resizeWindow({ width, height, x, y });
   });
   globalShortcut.register('CommandOrControl+Shift+S', () => {
     window.webContents.toggleDevTools();
@@ -22,15 +61,9 @@ export default async () => {
   });
   const preferencesText = fs.readFileSync(path.join(__dirname, '../../preferences.json'), 'utf-8');
   const preferences = JSON.parse(preferencesText);
-  Object.keys(preferences.hotkeys).forEach(hotkey => {
-    for (const key in preferences.hotkeys[hotkey]) {
-      if (globalShortcut.isRegistered(key)) {
-        console.error('Duplicate hotkey error');
-        return;
-      }
+  Object.keys(preferences.hotkeys).forEach(event => {
+    for (const hotkey of preferences.hotkeys[event]) {
+      registerKey(event, hotkey);
     }
-    globalShortcut.registerAll(preferences.hotkeys[hotkey], () => {
-      window.webContents.send(hotkey);
-    });
   });
 }
