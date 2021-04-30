85a86da744b081037b05a1b2aa609fc569f003af
unknown <guibasconti@gmail.com>
2021-04-30 02:38:50 -0400

    Add rune support

--- server/constants/events.ts
+++ server/constants/events.ts
@@ -1,4 +1,11 @@
 export default {
   WINDOW_RESIZE: 'window::resize',
-  POTION_COOLDOWN: 'cooldown::potion'
+  REGISTER_HOTKEY: 'hotkey::register',
+  ITEM_COOLDOWN: 'cooldown::item',
+  ATTACK_COOLDOWN: 'cooldown::attack',
+  HEALING_COOLDOWN: 'cooldown::healing',
+  SUPPORT_COOLDOWN: 'cooldown::support',
+  ATTACK_RUNE_COOLDOWN: 'cooldown::attack_rune',
+  HEALING_RUNE_COOLDOWN: 'cooldown::healing_rune',
+  SUPPORT_RUNE_COOLDOWN: 'cooldown::support_rune',
 };
--- server/core/preload.ts
+++ server/core/preload.ts
@@ -9,6 +9,29 @@ window.addEventListener('DOMContentLoaded', async () => {
     console.error('Wrong HTML file');
   }
 
+  const cooldowns = {
+    [events.ITEM_COOLDOWN]: {
+      active: false,
+      timer: 1000,
+      side: 'left'
+    },
+    [events.ATTACK_COOLDOWN]: {
+      active: false,
+      timer: 2000,
+      side: 'right'
+    },
+    [events.HEALING_COOLDOWN]: {
+      active: false,
+      timer: 1000,
+      side: 'left'
+    },
+    [events.SUPPORT_COOLDOWN]: {
+      active: false,
+      timer: 2000,
+      side: 'right'
+    },
+  };
+
   const sleep = (ms: number) => {
     return new Promise(resolve => setTimeout(resolve, ms));
   };
@@ -36,30 +59,108 @@ window.addEventListener('DOMContentLoaded', async () => {
   };
 
   const cooldownGenerator = async (timer: number = 1000, side: string = 'left') => {
-    const ticks = 100;
-    let currentCooldown = 100;
-    const { arc, arcFilled } = arcGenerator(side);
-    const selectedCooldown = side === 'left' ? cooldownsLeft : cooldownsRight;
-    selectedCooldown?.append(arc);
-    while (currentCooldown > 0) {
+    return new Promise<void>(async (resolve) => {
+      const ticks = 100;
+      let currentCooldown = 100;
+      const { arc, arcFilled } = arcGenerator(side);
+      const selectedCooldown = side === 'left' ? cooldownsLeft : cooldownsRight;
+      selectedCooldown?.append(arc);
+      while (currentCooldown > 0) {
+        arcFilled.style.height = `${currentCooldown}%`;
+        await sleep(timer / ticks);
+        currentCooldown -= 100 / ticks;
+      }
       arcFilled.style.height = `${currentCooldown}%`;
-      await sleep(timer / ticks);
-      currentCooldown -= 100 / ticks;
-    }
-    arcFilled.style.height = `${currentCooldown}%`;
-    selectedCooldown?.removeChild(arc);
+      selectedCooldown?.removeChild(arc);
+      return resolve();
+    });
   };
 
-  const potionCooldown = () => {
-    cooldownGenerator(1000, 'left');
+  const startCooldown = async (event: string, hotkey: string, registerKey: boolean = true) => {
+    return new Promise<void>(async (resolve) => {
+      if (!cooldowns[event]) {
+        return;
+      }
+      if (cooldowns[event].active) {
+        return;
+      }
+      cooldowns[event].active = true;
+      await cooldownGenerator(cooldowns[event].timer, cooldowns[event].side);
+      if (registerKey) {
+        ipcRenderer.invoke(events.REGISTER_HOTKEY, event, hotkey);
+      }
+      cooldowns[event].active = false;
+      return resolve();
+    });
+  };
+
+  // TODO: Implement item buffer
+  const itemCooldown = async (_event: any, hotkey: string, registerKey: boolean = true) => {
+    return new Promise<void>(async (resolve) => {
+      await startCooldown(events.ITEM_COOLDOWN, hotkey, registerKey);
+      return resolve();
+    });
+  };
+
+  const attackCooldown = async (_event: any, hotkey: string, registerKey: boolean = true) => {
+    return new Promise<void>(async (resolve) => {
+      await startCooldown(events.ATTACK_COOLDOWN, hotkey, registerKey);
+      return resolve();
+    });
+  };
+
+  const healingCooldown = async (_event: any, hotkey: string, registerKey: boolean = true) => {
+    return new Promise<void>(async (resolve) => {
+      await startCooldown(events.HEALING_COOLDOWN, hotkey, registerKey);
+      return resolve();
+    });
+  };
+
+  const supportCooldown = async (_event: any, hotkey: string, registerKey: boolean = true) => {
+    return new Promise<void>(async (resolve) => {
+      await startCooldown(events.SUPPORT_COOLDOWN, hotkey, registerKey);
+      return resolve();
+    });
+  };
+
+  const attackRuneCooldown = async (_event: any, hotkey: string) => {
+    return new Promise<void>(async (resolve) => {
+      itemCooldown(_event, hotkey, false);
+      await attackCooldown(_event, hotkey, false);
+      ipcRenderer.invoke(events.REGISTER_HOTKEY, events.ATTACK_RUNE_COOLDOWN, hotkey);
+      return resolve();
+    });
+  };
+
+  const healingRuneCooldown = async (_event: any, hotkey: string) => {
+    return new Promise<void>(async (resolve) => {
+      itemCooldown(_event, hotkey, false);
+      await healingCooldown(_event, hotkey, false);
+      ipcRenderer.invoke(events.REGISTER_HOTKEY, events.HEALING_RUNE_COOLDOWN, hotkey);
+      return resolve();
+    });
+  };
+
+  const supportRuneCooldown = async (_event: any, hotkey: string) => {
+    return new Promise<void>(async (resolve) => {
+      itemCooldown(_event, hotkey, false);
+      await supportCooldown(_event, hotkey, false);
+      ipcRenderer.invoke(events.REGISTER_HOTKEY, events.SUPPORT_RUNE_COOLDOWN, hotkey);
+      return resolve();
+    });
   };
 
   // Electron listeners
   ipcRenderer.on(events.WINDOW_RESIZE, (_event, { width, height }) => {
     resizeBody({ width, height });
   });
-  
-  ipcRenderer.on(events.POTION_COOLDOWN, potionCooldown);
+  ipcRenderer.on(events.ITEM_COOLDOWN, itemCooldown);
+  ipcRenderer.on(events.ATTACK_COOLDOWN, attackCooldown);
+  ipcRenderer.on(events.HEALING_COOLDOWN, healingCooldown);
+  ipcRenderer.on(events.SUPPORT_COOLDOWN, supportCooldown);
+  ipcRenderer.on(events.ATTACK_RUNE_COOLDOWN, attackRuneCooldown);
+  ipcRenderer.on(events.HEALING_RUNE_COOLDOWN, healingRuneCooldown);
+  ipcRenderer.on(events.SUPPORT_RUNE_COOLDOWN, supportRuneCooldown);
 
   startupInvokers();
 });
